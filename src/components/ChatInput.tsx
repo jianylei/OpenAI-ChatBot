36bc5c60db1ef7942dd6ab635c2e5be0d9461bf2
Jian yong Lei <jianyonglei@Jians-MacBook-Pro.local>
2023-06-11 18:56:11 -0400

    Added validator for Message type

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { cn } from '@/lib/utils'
+import { Message } from '@/lib/validators/message'
+import { useMutation } from '@tanstack/react-query'
+import { nanoid } from 'nanoid'
 import { FC, HTMLAttributes, useState } from 'react'
 import ReactTextareaAutosize from 'react-textarea-autosize'
 
@@ -9,11 +12,41 @@ interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
     const [input, setInput] = useState<string>('')
 
+    const {mutate: sendMessage, isLoading} = useMutation({
+        mutationFn: async (message: Message) => {
+            const response = await fetch('/api/message', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ messages: 'hello' })
+            })
+
+            return response.body
+        },
+        onSuccess: () => {
+            console.log("success")
+        }
+    })
+
   return <div {...props} className={cn('border-t border-zinz-300', className)}>
     <div className='relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none'>
         <ReactTextareaAutosize 
         rows={2}
         maxRows={4}
+        onKeyDown={e => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+
+                const message: Message = {
+                    id : nanoid(),
+                    isUserInput: true,
+                    text: input
+                }
+
+                sendMessage(message)
+            }
+        }}
         value={input}
         onChange={e => setInput(e.target.value)}
         autoFocus
